20234a429cf129b14091493cbd7706e7e9ad34c3
Clyde <alaricbartlett@hotmail.com>
2025-06-10 19:16:46 -0700

    Finished Hurry Up card

--- src/main/java/sparklemod/cards/uncommon/HurryUp.java
+++ src/main/java/sparklemod/cards/uncommon/HurryUp.java
@@ -6,6 +6,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import sparklemod.cards.BaseCard;
 import sparklemod.character.SparkleCharacter;
 import sparklemod.powers.SparkleHurryUpPower;
@@ -29,7 +30,8 @@ public class HurryUp extends BaseCard {
     }
 
     public void use (AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ApplyPowerAction(p, p, new SparkleHurryUpPower(p, AbstractDungeon.player.energy.energy, -1)));
+        int currentEnergy = EnergyPanel.getCurrentEnergy() - this.cost;
+        addToBot(new ApplyPowerAction(p, p, new SparkleHurryUpPower(p, currentEnergy, -1)));
         addToBot(new PressEndTurnButtonAction());
     }
 }
