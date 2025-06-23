1712363eca2c0f2011b1810e8f71e8eee2168c83
Clyde <alaricbartlett@hotmail.com>
2025-06-23 16:40:06 -0700

    Added OnceMoreWithFeeling card and strings

---
+++ src/main/java/sparklemod/cards/uncommon/OnceMoreWithFeeling.java
@@ -0,0 +1,43 @@
+package sparklemod.cards.uncommon;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import sparklemod.cards.BaseCard;
+import sparklemod.character.SparkleCharacter;
+import sparklemod.powers.MaskPower;
+import sparklemod.powers.UnexpectedPower;
+import sparklemod.util.CardStats;
+
+//Once More, With Feeling! - Attack, 1 energy. Gain Mask. Deal 5(7) damage. Gain 1 Unexpected.
+public class OnceMoreWithFeeling extends BaseCard {
+    public static final String ID = makeID(OnceMoreWithFeeling.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            SparkleCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+
+    private static final int BASE_DAMAGE = 5;
+    private static final int UPGRADE_DAMAGE = 2;
+
+    public OnceMoreWithFeeling() {
+        super(ID, info);
+
+        setCustomVar("OnceMoreWithFeelingDamage", BASE_DAMAGE, UPGRADE_DAMAGE);
+    }
+
+    public void use (AbstractPlayer p, AbstractMonster m) {
+        int numTimes = (p.hasPower(UnexpectedPower.POWER_ID) ? p.getPower(UnexpectedPower.POWER_ID).amount : 0);
+
+        for(int i=0; i < numTimes+1; i++) {
+            addToBot(new ApplyPowerAction(p, p, new MaskPower(p, 1)));
+            addToBot(new DamageAction(m, new DamageInfo(p, customVar("OnceMoreWithFeelingDamage"), DamageInfo.DamageType.NORMAL)));
+        }
+        addToBot(new ApplyPowerAction(p, p, new UnexpectedPower(p, 1)));
+    }
+}
