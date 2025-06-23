5f24870f7b61f75cd3040f6c22af72897159107a
Clyde <alaricbartlett@hotmail.com>
2025-06-23 13:27:27 -0700

    Revert "Renamed MaskEnergyGainPower to MaskPower"
    This reverts commit d4c13e8c93fb5d6010533c5a5923c874860365ce.

--- src/main/java/sparklemod/powers/MaskEnergyGainPower.java
+++ src/main/java/sparklemod/powers/MaskEnergyGainPower.java
@@ -9,14 +9,14 @@ import com.megacrit.cardcrawl.powers.AbstractPower;
 
 import static sparklemod.SparkleMod.makeID;
 
-public class MaskPower extends BasePower {
-    public static final String POWER_ID = makeID(MaskPower.class.getSimpleName());
+public class MaskEnergyGainPower extends BasePower {
+    public static final String POWER_ID = makeID(MaskEnergyGainPower.class.getSimpleName());
     private static final AbstractPower.PowerType TYPE = AbstractPower.PowerType.BUFF;
     private static final boolean TURN_BASED = false;
 
     private int energyGain = 0;
 
-    public MaskPower(AbstractCreature owner, int amount) {
+    public MaskEnergyGainPower(AbstractCreature owner, int amount) {
         super(POWER_ID, TYPE, TURN_BASED, owner, amount);
         energyGain += amount;
         updateDescription();
