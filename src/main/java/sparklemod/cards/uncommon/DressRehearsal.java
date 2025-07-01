fe2ac43a09dcf351e70612892935911705c7a227
Clyde <alaricbartlett@hotmail.com>
2025-07-01 15:23:22 -0700

    Added DressRehearsal card and strings.

---
+++ src/main/java/sparklemod/cards/uncommon/DressRehearsal.java
@@ -0,0 +1,34 @@
+package sparklemod.cards.uncommon;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.MetallicizePower;
+import sparklemod.cards.BaseCard;
+import sparklemod.character.SparkleCharacter;
+import sparklemod.util.CardStats;
+
+//Dress Rehearsal - power, 2(1) energy - Gain 3(4) Metallicize.
+public class DressRehearsal extends BaseCard {
+    public static final String ID = makeID(DressRehearsal.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            SparkleCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.POWER, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            2 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+
+    private static final int METALLICIZE_AMOUNT = 3;
+    private static final int METALLICIZE_UPGRADE_AMOUNT = 1;
+
+    public DressRehearsal () {
+        super(ID, info);
+        setCostUpgrade(1);
+        setCustomVar("DressRehearsalMetallicizeAmount", METALLICIZE_AMOUNT, METALLICIZE_UPGRADE_AMOUNT);
+    }
+
+    public void use (AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new MetallicizePower(p, customVar("DressRehearsalMetallicizeAmount"))));
+    }
+}
