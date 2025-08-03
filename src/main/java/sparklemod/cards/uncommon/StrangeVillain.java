d1c75a8992c87c67d06a5bbaa3d92b4b9436de26
Clyde <alaricbartlett@hotmail.com>
2025-08-02 19:58:02 -0700

    Rebalance for StrangeVillain. Upgrade now heals the player and damages all enemies, then exhausts.

--- src/main/java/sparklemod/cards/uncommon/StrangeVillain.java
+++ src/main/java/sparklemod/cards/uncommon/StrangeVillain.java
@@ -1,6 +1,8 @@
 package sparklemod.cards.uncommon;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -10,7 +12,7 @@ import sparklemod.cards.BaseCard;
 import sparklemod.character.SparkleCharacter;
 import sparklemod.util.CardStats;
 
-//Strange Villain - attack, 0 energy - Randomly heal 3 or deal 5 damage to yourself and each enemy. (Heal 3. Randomly heal 3 or deal 7 damage to each enemy.)
+//Strange Villain - attack, 0 energy - Randomly heal 3 or deal 5 damage to yourself and each enemy. (Heal 3. Deal 7 damage to all enemies. Exhaust.)
 public class StrangeVillain extends BaseCard {
     public static final String ID = makeID(StrangeVillain.class.getSimpleName());
     private static final CardStats info = new CardStats(
@@ -21,38 +23,47 @@ public class StrangeVillain extends BaseCard {
             0 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
 
+    private static final int BASE_DAMAGE = 5;
+    private static final int DAMAGE_UPGRADE = 2;
+    private static final int HEAL_AMOUNT = 3;
+
     public StrangeVillain () {
         super(ID, info);
 
         this.upgradesDescription = true;
+        this.setExhaust(false, true);
+
+        setMagic(HEAL_AMOUNT);
+        setDamage(BASE_DAMAGE, DAMAGE_UPGRADE);
     }
 
     public void use (AbstractPlayer p, AbstractMonster m) {
-
-        //handle the player first
         if(this.upgraded) {
-            addToBot(new HealAction(p, p, 3));
+            addToBot(new HealAction(p, p, HEAL_AMOUNT));
+            addToBot(new DamageAllEnemiesAction(p, this.damage, DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.SMASH));
         }
         else {
+            //handle player
             int choice = randomIntWithoutVariance(1, 2);
             if(choice == 1) {
-                addToBot(new HealAction(p, p, 3));
+                addToBot(new HealAction(p, p, HEAL_AMOUNT));
             }
             else {
                 addToBot(new DamageAction(p, new DamageInfo(p, 5, DamageInfo.DamageType.NORMAL)));
             }
-        }
-        //handle monsters
-        for(AbstractMonster mo: AbstractDungeon.getMonsters().monsters) {
-            if(!mo.isDeadOrEscaped()) {
-                int choice = randomIntWithoutVariance(1, 2);
-                if(choice == 1) {
-                    addToBot(new HealAction(mo, p, 3));
-                }
-                else {
-                    addToBot(new DamageAction(mo, new DamageInfo(p, (this.upgraded ? 7 : 5), DamageInfo.DamageType.NORMAL)));
+            //handle monsters
+            for(AbstractMonster mo: AbstractDungeon.getMonsters().monsters) {
+                if(!mo.isDeadOrEscaped()) {
+                    choice = randomIntWithoutVariance(1, 2);
+                    if(choice == 1) {
+                        addToBot(new HealAction(mo, p, HEAL_AMOUNT));
+                    }
+                    else {
+                        addToBot(new DamageAction(mo, new DamageInfo(p, (this.damage), DamageInfo.DamageType.NORMAL)));
+                    }
                 }
             }
         }
+
     }
 }
